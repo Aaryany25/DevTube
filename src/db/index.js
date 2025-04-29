97fd8a331dcc5aa101ed36247a5f0a0afe21bab2
Aaryan <aaryany.2505@gmail.com>
2025-04-29 14:38:31 +0530

    Donnected mongo database successfully

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import { DB_Name } from "../constants.js"
+import dotenv from "dotenv";
+
+dotenv.config({path:'./.env'})
+
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/${DB_Name}`)
+        console.log(`\n MongoDb connection on DB host : ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MongoDB connection error :",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({path:'./env'})
+connectDB()
+/*
+import express from "express"
+
+const app = express()
+(async ()=>{
+try{
+   await mongoose.connect(`${process.env.MONGO_URL}/${DB_Name}`)
+   app.on("Error",(error)=>{
+    console.log("Error",error)
+    throw error
+   })
+   app.listen(process.env.PORT,()=>{
+    console.log("Server Started")
+   })
+}
+catch(error){
+    console.error("Error: ",error)
+}
+})()
+*/
